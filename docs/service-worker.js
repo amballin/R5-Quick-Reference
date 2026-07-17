c752c2dcc1b60913e20460d14896ccb2908a6940
Andy Ballin <andy@nefs-mbp-16.mynetworksettings.com>
2026-07-17 09:11:26 -0400

    Refine subject card shooting guidance

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "photography-reference";
-const CACHE_NAME = "photography-reference-1784234177";
+const CACHE_NAME = "photography-reference-1784283398";
 const CACHE_URLS = [
   "./",
   ".nojekyll",
