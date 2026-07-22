b30c6e64644f192cb2cbce281c7dd7aad36f7b81
Andy Ballin <andy@nefs-mbp-16.mynetworksettings.com>
2026-07-22 12:04:34 -0400

    Update R5 reference

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "photography-reference";
-const CACHE_NAME = "photography-reference-1784641998";
+const CACHE_NAME = "photography-reference-1784736253";
 const CACHE_URLS = [
   "./",
   ".nojekyll",
