c6d9a1dccb314f64a6ca5bde7947112f16bf8f48
Andy <andy.ballin@gmail.com>
2026-08-24 10:20:40 -0400

    Refresh main verification downloads

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "photography-reference";
-const CACHE_NAME = "photography-reference-1787579387";
+const CACHE_NAME = "photography-reference-1787580565";
 const CACHE_URLS = [
   "./",
   ".nojekyll",
